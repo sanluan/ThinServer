df63d2946d749b69b4a80e9498ed717585488ff4
sanluan <master@sanluan.com>
2017-04-17 09:43:13 +0800

    update

--- bin/startFtp.bat
+++ bin/startFtp.bat
@@ -1,3 +1,3 @@
 @echo off
 call setclasspath.bat
-%EXECUTABLE% -classpath "%CLASSPATH%" com.sanluan.server.ThinFtpServer
+%EXECUTABLE% -classpath "%CLASSPATH%" com.sanluan.server.ThinFtpServer>%cd%/logs/out.log
